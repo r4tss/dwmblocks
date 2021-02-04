a3a694a15385da8955fa3316c7f6cd131c5ac6a1
r4tss <68610647+r4tss@users.noreply.github.com>
2021-02-04 11:19:26 +0100

    Add files via upload

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/							/*Update Interval*/	/*Update Signal*/
-	{"",		"bash $HOME/scripts/volume.sh",					0,			2},
+	{"", 		"bash $HOME/.scripts/musicBlock.sh get status",			1,			1},
 
-	{"", 		"bash $HOME/scripts/musicBlock.sh get status",			1,			1},
+	{";",		"",								0,			0},
 
-	{"", 		"date +'%a %d %b %H:%M '",					5,			0},
+	{"",		"bash $HOME/.scripts/volume.sh",				0,			2},
+
+	{"",		"cat /tmp/scripts/alarm",					0,			3},
+
+	{"",		"bash $HOME/.scripts/date.sh",					5,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "    ";
 static unsigned int delimLen = 5;
